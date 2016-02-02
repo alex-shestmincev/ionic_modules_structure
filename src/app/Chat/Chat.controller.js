e3779d495bbe16cb3f842543b1ca586c65dd8ec7
alex.shestmitsev <alex.shestmintsev@altexsoft.com>
2016-02-02 09:48:26 +0200

    Fix gitignore, gulpfile

--- src/app/Chat/Chat.controller.js
+++ src/app/Chat/Chat.controller.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers')
-  .controller('ChatCtrl', function($scope, Chats) { console.log('ChatCtr1123ghfg');
+  .controller('ChatCtrl', function($scope, Chats) { console.log('DashCtrl');
     $scope.chats = Chats.all();
     $scope.remove = function(chat) {
       Chats.remove(chat);
--- src/app/controllers.js
+++ src/app/controllers.js
@@ -1,7 +1,5 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
-
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
